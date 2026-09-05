e4245845811ee8724afd5103057ab2a2a700b953
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-09-05 16:41:36 +0530

    solve bug in database page

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -9,7 +9,7 @@ const isRemote =
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  proces1s.env.DB_PASSWORD,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT) || 5432,
